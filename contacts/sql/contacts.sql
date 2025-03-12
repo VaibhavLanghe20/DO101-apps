8508e17db6e13f544b66d729527bf89d09ee9fc1
VaibhavLanghe20 <vaibhav.langhe23@pcu.edu.in>
2025-03-12 12:21:46 +0530

    Update contacts.sql

--- contacts/sql/contacts.sql
+++ contacts/sql/contacts.sql
@@ -1,6 +1,8 @@
 drop table if exists contact;
 
+
 create table contact(id serial primary key,firstname varchar(30) not null,lastname varchar(30) not null, email varchar(30) not null);
+
 
 insert into contact(firstname, lastname, email) values
   ('Bilbo','Baggins','bilbo@theshire.com'),
